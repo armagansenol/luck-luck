c8baa300739bf5244cd2bc9ba5d30f2b46a6ca00
armagansenol <armagansnl@gmail.com>
2025-09-26 16:17:37 +0300

    feat: add address section to footer and adjust footer styling

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,26 +4,33 @@ import cx from "clsx"
 
 export default function Footer() {
   return (
-    <footer
-      className={cx(
-        s.footer,
-        "flex flex-col tablet:flex-row items-center tablet:items-end justify-between gap-5 tablet:gap-0"
-      )}
-    >
-      <small>© Stork Motion 2025</small>
+    <>
+      <div className={cx(s.address)}>
+        <div className='flex justify-center desktop:justify-start text-center desktop:text-left w-full'>
+          FSM Mah., Balkan Cad., <br /> JUSTWork Ofis Kampüs, No: 62A, 34770, <br /> Ümraniye, İstanbul / Türkiye
+        </div>
+      </div>
+      <footer
+        className={cx(
+          s.footer,
+          "flex flex-col tablet:flex-row items-center tablet:items-end justify-between gap-5 tablet:gap-0"
+        )}
+      >
+        <small>© Stork Motion 2025</small>
 
-      {/* <div className={cx(s.social, "flex items-center justify-center gap-5")}>
+        {/* <div className={cx(s.social, "flex items-center justify-center gap-5")}>
         <Link to="/" className={s.iconC}>
           <IconLinkedin fill="var(--black)" />
         </Link>
       </div> */}
 
-      <small>
-        Web Solutions Partner{" "}
-        <a className="underline" href="https://bytemywork.com">
-          Byte My Work
-        </a>
-      </small>
-    </footer>
+        <small>
+          Web Solutions Partner{" "}
+          <a className='underline' href='https://bytemywork.com'>
+            Byte My Work
+          </a>
+        </small>
+      </footer>
+    </>
   )
 }
